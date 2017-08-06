fa40dc9eb29749ea68f56ac490e5cba6ab8b32c4
hezhengfu <hezhengfu@feinno.com>
2017-08-06 22:42:06 +0800

    。

---
+++ src/components/header/header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <div class="headerContaner">
+       {{msg}}
+     </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "header",
+  data (){
+    return{
+      msg:"header"
+    }
+
+  }
+}
+</script>
+<style  scoped>
+     .header{
+       width: 750px;
+       height: 100px;
+       display: flex;
+       justify-content: center;
+       align-items: center;
+       text-align: center;
+       font-size: 68px;
+       background: rgba(0, 0, 0, 0.5);
+     }
+</style>
