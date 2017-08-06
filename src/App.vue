57e791222bf5d81703e5cbb77639f10a6b00fcc2
hezhengfu <hezhengfu@feinno.com>
2017-08-06 22:31:16 +0800

    。。。

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <div id="hello">
-    <img src="./assets/logo.png">
-    <Hello></Hello>
+  <div id="myApp">
+      <header-content></header-content>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+
+import headerContent from './components/header/header';
 export default {
-  components: {
-    Hello
-  }
+  name:'app',
+   components:{
+     headerContent
+   }
 }
 </script>
 
 <style>
-#hello {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#myApp {
+  width: 750px;
+  overflow:hidden;
+}
+html,body{
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import FastClick from 'fastclick'
-import store from './store/'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import FastClick from 'fastclick';
+import store from './store/';
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function() {
         FastClick.attach(document.body);
     }, false);
 };
+(function () {
+  var scalar = 750,
+  scale = parseInt(window.screen.width) / scalar,
+  userScalable = 'no';
+  var val = scale.toFixed(2) * 1;
+  document.getElementById('viewport').setAttribute('content', 'target-densitydpi=device-dpi,width=750,user-scalable=no,initial-scale=' +val +',minimum-scale=' +val +', maximum-scale=' +val+'');
+  })();
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
