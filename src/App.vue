d139e77f2eb639614fa987245a35f5c3f29c3961
hezhengfu <hezhengfu@feinno.com>
2017-08-06 21:07:02 +0800

    VUE 基本建构

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="hello">
+    <img src="./assets/logo.png">
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#hello {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
